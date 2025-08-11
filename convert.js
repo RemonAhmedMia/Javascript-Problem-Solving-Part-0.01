70518fd14b5aa69f7df1065cd211a39ed5fa9a78
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-11 11:04:31 +0100

    convertion js

--- convert.js
+++ convert.js
@@ -4,3 +4,27 @@ function inchToFeet(inch){
 }
 const rimoHeight = inchToFeet(75);
 console.log(rimoHeight); //Output:6.25feet
+
+
+function inchToFeet2(inch) {
+    const freeFranction = inch / 12 ;
+    const feetNumber = parseInt(freeFranction);
+    const inchRemaining = inch % 12;
+    const result = feetNumber + " ft " + inchRemaining + " inch " ;
+    return result;
+}
+const rimonHeight = inchToFeet2(75);
+console.log(rimonHeight); //Output: 6ft 3 inch
+
+
+
+// Mile To km Convert=============
+
+//1mile = 1.60934 km;
+
+function mileToKilometer(mile) {
+    const kilo = mile / 1.60934;
+    return kilo;
+}
+const output = mileToKilometer(10);
+console.log(output);
