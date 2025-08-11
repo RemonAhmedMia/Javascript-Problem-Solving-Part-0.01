17b2eeab1b4c76bbfb2937fdf3b1ccc929a86f2a
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-11 19:33:22 +0100

    mathmetics-02

--- mathemetics.js
+++ mathemetics.js
@@ -7,3 +7,12 @@ console.log(max);
 console.log(Math.abs(50-88)); //33
 
 console.log(Math.round(2.95)); //3
+
+console.log(Math.floor(2.999999)); //2; doshomik er por joto beshi man e thakok na keno sob somoy nicher diker porno songkha k dhorbe
+
+console.log(Math.ceil(2.0001)); //3, onekta celiing fan like joto kom thakok na keno sob somoy uporer porno songkha k dhorbe;
+
+console.log(Math.random()); //prothi refresh e noton man dibe 1 er vitor
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
