010e30bd74195561851b9073a354429fa4ccf30f
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-11 18:19:32 +0100

    add-odd-averege

---
+++ odd-averege.js
@@ -0,0 +1,24 @@
+function oddAverege(numbers) {
+    const odds = []; //odd number gula k ekta array er moddhe rakhar jonno;
+    for(const number of numbers) {
+        if (number % 2 === 1) {
+            //console.log(number); bijor songkha gula ber korar jonno
+            odds.push(number); //odd er moddhe number k push kore dilam,orthat amader joto gula odd number ashbe segula k amra ekta array er moddhe dhukailam
+        }
+    }
+    //shodu matro odd numbers gula ekhane pawua jabe
+    //console.log(odds);
+    let sum = 0;
+    for(const number of odds){
+        sum = sum + number;
+    }
+    const count = odds.length;
+    console.log(sum , count);
+    //ekhon ghor ber korbo: sum/ koita upadan ache
+    const avg = sum / count ;
+    return avg
+    
+}
+const numbers = [45,67,56,51,80,81,91,90,101,21,11,9,49];
+const avg = oddAverege(numbers);
+console.log(avg);
