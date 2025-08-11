ec5ce286931c58dac966f8b5bbb3e0973d1e4928
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-11 19:28:32 +0100

    mathemetics-01

---
+++ mathemetics.js
@@ -0,0 +1,9 @@
+const min = Math.min(23,45,9,1,78,-90,89);
+console.log(min); //Output:-90
+
+const max = Math.max(23,45,9,1,78,-90,89);
+console.log(max);
+
+console.log(Math.abs(50-88)); //33
+
+console.log(Math.round(2.95)); //3
